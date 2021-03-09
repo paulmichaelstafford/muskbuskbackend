7ffeec2d4909dfed52e8b10a375cf2cfffcb924e
Paul Stafford <paul.stafford@visual-meta.com>
2021-03-09 22:51:11 +0100

    returning UserDAO for auth controller

--- src/main/java/com/mustbusk/backend/controller/fe/AuthController.java
+++ src/main/java/com/mustbusk/backend/controller/fe/AuthController.java
@@ -2,18 +2,31 @@ package com.mustbusk.backend.controller.fe;
 
 import java.security.Principal;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mustbusk.backend.app.model.user.UserDAO;
+import com.mustbusk.backend.app.model.user.UserService;
+
 @RestController
 public class AuthController
 {
+
+	private final UserService userService;
+
+	@Autowired
+	public AuthController(UserService userService)
+	{
+		this.userService = userService;
+	}
+
 	@CrossOrigin()
 	@RequestMapping(value = "/auth/user", method = RequestMethod.GET)
-	public Principal user(Principal user)
+	public UserDAO user(Principal user)
 	{
-		return user;
+		return userService.findUserByEmail(user.getName());
 	}
 }
